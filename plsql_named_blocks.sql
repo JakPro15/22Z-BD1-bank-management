80f8954c5dd82e67ca9acd75b3a3d252555bffe8
Kamil Michalak <01169209@pw.edu.pl>
2023-01-20 01:34:59 +0100

    added procedures for making inside and outside transactions, also procedure for closing accounts that dont pay loans

--- plsql_named_blocks.sql
+++ plsql_named_blocks.sql
@@ -32,6 +32,7 @@ END;
 SELECT client_id, monthly_card_payment(client_id)
 FROM CLIENTS;
 
+
 -- Funkcja oblicza ogólny balans w PLN wszystkich kont klienta, wliczaj¹c po¿yczki i lokaty.
 CREATE OR REPLACE FUNCTION calculate_total_balance(p_client_id NUMBER)
 RETURN NUMERIC
@@ -94,65 +95,141 @@ INNER JOIN ACCOUNTS USING(account_id)
 ORDER BY outside_transactions_history.outside_transaction_id;
 
 
-
 --procedura odpowiedzialna za wziêcie po¿yczki i dodania pieniêdzy do konta
 CREATE OR REPLACE PROCEDURE take_loan (v_starting_amount NUMBER, v_date_due DATE, 
     v_yearly_interest_rate NUMBER, v_account_currency_id NUMBER)
 AS
-    v_current_balance INTEGER;
 BEGIN
-    SELECT balance INTO v_current_balance FROM ACCOUNT_CURRENCIES WHERE account_currency_id = v_account_currency_id;
-
     UPDATE ACCOUNT_CURRENCIES
-    SET balance = v_current_balance + v_starting_amount
+    SET balance = balance + v_starting_amount
     WHERE account_currency_id = v_account_currency_id;
 
-    INSERT INTO LOANS VALUES (NULL, v_starting_amount, v_starting_amount, SYSDATE, v_date_due, 
-        v_yearly_interest_rate, v_account_currency_id);
+    INSERT INTO LOANS VALUES (NULL, v_starting_amount, v_starting_amount, SYSDATE, 
+        v_date_due, v_yearly_interest_rate, v_account_currency_id);
     
 END;
 /
+
 
 --procedura odpowiedzialna za za³o¿enie lokaty i pobrania pieniedzy z konta
 CREATE OR REPLACE PROCEDURE make_investment (v_blocked_until DATE, v_yearly_interest_rate NUMBER,
     v_amount NUMBER, v_account_currency_id NUMBER)
 AS
-    v_current_balance INTEGER;
+    v_current_balance NUMBER;
 BEGIN
-    SELECT balance INTO v_current_balance FROM ACCOUNT_CURRENCIES WHERE account_currency_id = v_account_currency_id;
+    SELECT balance INTO v_current_balance 
+    FROM ACCOUNT_CURRENCIES 
+    WHERE account_currency_id = v_account_currency_id;
     
-    IF v_current_balance - v_amount > 0
+    IF v_current_balance - v_amount >= 0
     THEN
         UPDATE ACCOUNT_CURRENCIES
-        SET balance = v_current_balance - v_amount
+        SET balance = balance - v_amount
         WHERE account_currency_id = v_account_currency_id;
         
-        INSERT INTO INVESTMENTS VALUES (NULL, SYSDATE, NULL, v_blocked_until, v_yearly_interest_rate,
-            v_amount, v_account_currency_id); 
+        INSERT INTO INVESTMENTS VALUES (NULL, SYSDATE, NULL, v_blocked_until, 
+            v_yearly_interest_rate, v_amount, v_account_currency_id); 
     END IF;
 END;
 /
 
 
---procedura, która ustawia datê zamkniêcia konta klienta na miesi¹c do przodu,
---jesli klient przez rok nie sp³aci³ wiêcej 10% swojej po¿yczki
+--procedura odpowiedzialna za robienie przelewów wewnêtrznych
+CREATE OR REPLACE PROCEDURE make_inside_transaction (v_amount NUMBER, v_acc_from_id NUMBER, v_acc_to_id NUMBER)
+AS
+    v_current_sender_balance NUMBER;
+    v_sender_currency_rate NUMBER;
+    v_receiver_currency_rate NUMBER;
+    v_amount_after_convert NUMBER;
+BEGIN
+    SELECT balance INTO v_current_sender_balance 
+    FROM ACCOUNT_CURRENCIES 
+    WHERE account_currency_id = v_acc_from_id;
+    
+    IF v_current_sender_balance - v_amount >= 0
+    THEN
+        SELECT c.exchange_rate_to_pln INTO v_sender_currency_rate
+        FROM CURRENCIES c JOIN ACCOUNT_CURRENCIES ac ON c.short_name = ac.currency_short_name
+        WHERE ac.account_currency_id = v_acc_from_id;
+        
+        SELECT c.exchange_rate_to_pln INTO v_receiver_currency_rate
+        FROM CURRENCIES c JOIN ACCOUNT_CURRENCIES ac ON c.short_name = ac.currency_short_name
+        WHERE ac.account_currency_id = v_acc_to_id;
+        
+        v_amount_after_convert := v_current_sender_balance / v_sender_currency_rate * v_receiver_currency_rate;
+    
+        UPDATE ACCOUNT_CURRENCIES
+        SET balance = balance - v_amount
+        WHERE account_currency_id = v_acc_from_id;
+        
+        INSERT INTO INSIDE_TRANSACTIONS_HISTORY VALUES (NULL, v_amount, 
+            v_amount_after_convert, SYSDATE, v_acc_from_id, v_acc_to_id);
+        
+        UPDATE ACCOUNT_CURRENCIES
+        SET balance = balance + v_amount_after_convert
+        WHERE account_currency_id = v_acc_to_id;
+    END IF;
+END;
+/
 
 
---funkcja, która dla podanej waluty zrobi jej "bilans" u¿ycia
---suma w po¿yczkach, suma w inwestycjach, suma w przelewach wewn. i zewn.
---mo¿na po czymœ pogrupowaæ jeszcze
+--procedura odpowiedzialna za wykonywanie przelewu "na zewnatrz" (do innego banku)
+CREATE OR REPLACE PROCEDURE make_outside_transaction (v_outside_acc_number NUMBER, 
+    v_amount NUMBER, v_inside_currency_id NUMBER)
+AS
+    v_current_sender_balance NUMBER;
+BEGIN
+    SELECT balance INTO v_current_sender_balance 
+    FROM ACCOUNT_CURRENCIES 
+    WHERE account_currency_id = v_inside_currency_id;
+    
+    IF v_current_sender_balance + v_amount >= 0
+    THEN
+        UPDATE ACCOUNT_CURRENCIES
+        SET balance = balance + v_amount
+        WHERE account_currency_id = v_inside_currency_id;
+    
+        INSERT INTO OUTSIDE_TRANSACTIONS_HISTORY VALUES (NULL, v_outside_acc_number, 
+            v_amount, SYSDATE, v_inside_currency_id);
+    END IF;
+END;
+/
 
 
---funkcja zwi¹zana z obliczaniem czegoœ w kredytach i/lub po¿yczkach
+--procedura, która zamyka konto klienta,jesli klient przez rok nie sp³aci³ wiêcej 10% swojej po¿yczki
+CREATE OR REPLACE PROCEDURE close_unpaid_accounts
+AS
+    v_loan_paid NUMBER;
+    v_time_passed NUMBER;
+    CURSOR cr is SELECT * FROM LOANS;
+    v_rec_loans LOANS%ROWTYPE;
+BEGIN
+
+    OPEN cr;
+    LOOP
+        EXIT WHEN cr%NOTFOUND;
+        FETCH cr INTO v_rec_loans;
+        
+        v_loan_paid := v_rec_loans.starting_amount - v_rec_loans.current_amount;
+        v_time_passed := SYSDATE - v_rec_loans.date_taken;
+        
+        IF v_time_passed > 365 AND v_loan_paid <= 0.1 * v_rec_loans.starting_amount
+        THEN
+            UPDATE ACCOUNTS
+            SET closing_date = SYSDATE
+            WHERE account_id in (SELECT a.account_id 
+                FROM ACCOUNTS a 
+                JOIN ACCOUNT_CURRENCIES ac ON a.account_id = ac.account_id
+                JOIN LOANS l ON ac.account_currency_id = l.account_currency_id
+                WHERE l.loan_id = v_rec_loans.loan_id);
+        END IF;
+    END LOOP;
+    CLOSE cr;
+
+END;
+/
 
 
---procedura przydzielaj¹ca bonusy finansowe top 3 najaktywniejszym klientom
-
-
-
-
-
-
-
+--wyzwalacz, który sprawdza, czy osoba zak³adaj¹ca konto ukoñczy³a 18 lat
 
 
